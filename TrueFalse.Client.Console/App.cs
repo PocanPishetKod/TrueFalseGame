a45c5a674ec36ff2ed12d9feba50b8dbb38ab858
PocanPishetKod <themarcus77@yandex.ru>
2020-12-19 23:11:23 +0300

    Добавил заготовку консольный клиент для тестирования клиентской логики

---
+++ TrueFalse.Client.Console/App.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrueFalse.Client.Domain;
+using TrueFalse.SignalR.Client.Dtos;
+
+namespace TrueFalse.Client.ConsoleApp
+{
+    public class App
+    {
+        private const string SavesPath = @"C:\TrueFalse saves";
+
+        public App()
+        {
+            SubscrybeOnEvents();
+        }
+
+        public void SubscrybeOnEvents()
+        {
+
+        }
+
+        private void OnReceiveGameTables(ReceiveGameTablesParams @params)
+        {
+            foreach (var item in @params.GameTables)
+            {
+                Console.WriteLine($"Id = {item.Id} | Name = {item.Name}");
+            }
+        }
+
+        private void OnReceiveNewGameTable(ReceiveNewGameTableParams @params)
+        {
+
+        }
+
+        private string InputNameProcess()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите ник:");
+
+                var result = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(result))
+                {
+                    Console.WriteLine("Введите корректный ник");
+                }
+                else
+                {
+                    return result;
+                }
+            }
+        }
+
+        private void RequestGameTablesProcess()
+        {
+            while (true)
+            {
+                Console.WriteLine("Показать игровые столы? (Да/Нет)");
+                var input = Console.ReadLine();
+                if (input.ToUpper() == "ДА")
+                {
+                    //_application.RequestGameTables();
+                    break;
+                }
+            }
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                var playerName = InputNameProcess();
+                //_application.Authenticate(playerName).Wait();
+                RequestGameTablesProcess();
+
+                Console.WriteLine("Нажмите любую клавишу, чтобы выйти");
+                Console.ReadLine();
+                break;
+            }
+        }
+    }
+}
---
+++ TrueFalse.Client.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrueFalse.Client.ConsoleApp
+{
+    class Program
+    {
+        private static App _app;
+
+        static void Main(string[] args)
+        {
+            _app = new App();
+            _app.Start();
+        }
+    }
+}
