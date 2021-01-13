4ad2c50604185d881028eaa3f1bc050c99c23a39
PocanPishetKod <themarcus77@yandex.ru>
2021-01-13 21:15:06 +0300

    Добавил интерфейс сервиса для блокирования UI и интерфейс диспетчера

---
+++ TrueFalse.Client.Domain/Interfaces/IDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrueFalse.Client.Domain.Interfaces
+{
+    public interface IDispatcher
+    {
+        void Invoke(Action action);
+    }
+}
