95196329f497a19373876795f3a5b7b46a694f31
PocanPishetKod <themarcus77@yandex.ru>
2021-03-29 20:59:45 +0300

    Отнаследовал PlayingCard от BaseModel и добавил свойство

--- TrueFalse.Client.Domain1/Models/Cards/PlayingCard.cs
+++ TrueFalse.Client.Domain1/Models/Cards/PlayingCard.cs
@@ -6,12 +6,23 @@ using System.Threading.Tasks;
 
 namespace TrueFalse.Client.Domain.Models.Cards
 {
-    public class PlayingCard
+    public class PlayingCard : BaseModel
     {
         public int Id { get; set; }
 
         public PlayingCardSuit? Suit { get; set; }
 
         public PlayingCardRank? Rank { get; set; }
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                _isSelected = value;
+                OnPropertyChanged(nameof(IsSelected));
+            }
+        }
     }
 }
